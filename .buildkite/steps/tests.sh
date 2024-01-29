eb848f4bf65a990e7b8049f2332026173e5e58ac
Josh Deprez <jd@buildkite.com>
2024-01-29 17:43:25 +1100

    Add pipeline
    something something "great artists steal", so I copied the parts from buildkite-agent that:
    * checks go.mod is tidy
    * checks all go source code has been formatted with gofmt
    * runs tests
    
    This happens inside a Go container specified with Dockerfile-compile and that is used by docker-compose.yml and _that_ is used by the docker-compose plugin in pipeline.yml.

---
+++ .buildkite/steps/tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+go version
+echo arch is "$(uname -m)"
+
+go install gotest.tools/gotestsum@v1.8.0
+
+echo '+++ Running tests'
+gotestsum --junitfile "junit-${BUILDKITE_JOB_ID}.xml" -- -count=1 -coverprofile=cover.out -failfast "$@" ./...
+
+echo 'Producing coverage report'
+go tool cover -html cover.out -o cover.html
