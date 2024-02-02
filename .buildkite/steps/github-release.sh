f23b7fb5905d724bce4a715153054acf45aacffa
Naufan Rizal <naufan@buildkite.com>
2024-02-02 17:03:58 +1100

    feat: skip release if tag already exists

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -2,6 +2,12 @@
 
 agent_version=$(cat version/VERSION)
 
+# Skip release if version already exists
+if git ls-remote --tags origin | grep "refs/tags/v${agent_version}" ; then
+  echo "Version ${agent_version} already exists"
+  exit 0
+fi
+
 echo "--- :package: Downloading built binaries from build ${ARTIFACTS_BUILD_ID}"
 buildkite-agent artifact download --build "$ARTIFACTS_BUILD_ID" "pkg/test-splitter-*" .
 
