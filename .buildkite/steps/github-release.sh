1e27e2468fdedf64d92f04ba80d528e905b0bd6c
Naufan Rizal <naufan@buildkite.com>
2024-02-02 17:05:57 +1100

    feat: use gh cli to create release

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -12,3 +12,4 @@ echo "--- :package: Downloading built binaries from build ${ARTIFACTS_BUILD_ID}"
 buildkite-agent artifact download --build "$ARTIFACTS_BUILD_ID" "pkg/test-splitter-*" .
 
 echo "--- :octocat: :rocket: Creating GitHub release for v$agent_version"
+gh release create "v$agent_version" ./pkg/* --generate-notes
