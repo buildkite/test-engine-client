4d97566c38bf5726a175df90e24a151d56f7fa52
Naufan Rizal <naufan@buildkite.com>
2024-02-06 11:32:31 +1100

    chore: apply bash style suggestion

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-agent_version=$(cat version/VERSION)
+set -euo pipefail
+
+splitter_version="$(cat version/VERSION)" 
 
 # Skip release if version already exists
-if git ls-remote --tags origin | grep "refs/tags/v${agent_version}" ; then
-  echo "Version ${agent_version} already exists"
+if git ls-remote --tags origin | grep "refs/tags/v${splitter_version}" ; then
+  echo "Version ${splitter_version} already exists"
   exit 0
 fi
 
 echo "--- :package: Downloading built binaries from build ${ARTIFACTS_BUILD_ID}"
-buildkite-agent artifact download --build "$ARTIFACTS_BUILD_ID" "pkg/test-splitter-*" .
+buildkite-agent artifact download --build "${ARTIFACTS_BUILD_ID}" "pkg/test-splitter-*" .
 
-echo "--- :octocat: :rocket: Creating GitHub release for v$agent_version"
-gh release create "v$agent_version" ./pkg/* --generate-notes
+echo "--- :octocat: :rocket: Creating GitHub release for v$version"
+gh release create "v${splitter_version}" ./pkg/* --generate-notes
--- .buildkite/steps/upload-release-step.sh
+++ .buildkite/steps/upload-release-step.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
-version=$(cat version/VERSION)
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+splitter_version="$(cat version/VERSION)"
 
 cat <<YAML | buildkite-agent pipeline upload
 steps:
   - name: ":rocket: Release"
     trigger: "test-splitter-client-release"
     build:
-      message: "Release for ${version}, build ${BUILDKITE_BUILD_NUMBER}"
+      message: "Release for ${splitter_version}, build ${BUILDKITE_BUILD_NUMBER}"
       commit: "${BUILDKITE_COMMIT}"
       branch: "${BUILDKITE_BRANCH}"
       env:
