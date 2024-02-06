6df9203cdadf0de105199e1664bf9424363da820
Naufan Rizal <naufan@buildkite.com>
2024-02-06 11:52:47 +1100

    test: test the pipeline

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -14,4 +14,4 @@ echo "--- :package: Downloading built binaries from build ${ARTIFACTS_BUILD_ID}"
 buildkite-agent artifact download --build "${ARTIFACTS_BUILD_ID}" "pkg/test-splitter-*" .
 
 echo "--- :octocat: :rocket: Creating GitHub release for v$version"
-gh release create "v${splitter_version}" ./pkg/* --generate-notes
+gh release create "v${splitter_version}" ./pkg/* --generate-notes --draft
