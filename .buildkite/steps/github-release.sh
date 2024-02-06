ed7c5b027505cb59f3ce7a0d7a0440fc9dfcf7d2
Naufan Rizal <naufan@buildkite.com>
2024-02-06 12:13:27 +1100

    Revert "test: test the pipeline"
    This reverts commit 6df9203cdadf0de105199e1664bf9424363da820.

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -14,4 +14,4 @@ echo "--- :package: Downloading built binaries from build ${ARTIFACTS_BUILD_ID}"
 buildkite-agent artifact download --build "${ARTIFACTS_BUILD_ID}" "pkg/test-splitter-*" .
 
 echo "--- :octocat: :rocket: Creating GitHub release for v${splitter_version}"
-gh release create "v${splitter_version}" ./pkg/* --generate-notes --draft
+gh release create "v${splitter_version}" ./pkg/* --generate-notes
