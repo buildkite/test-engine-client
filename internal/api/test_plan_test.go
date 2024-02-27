f4a03ae14c6db227a38fc6898659c1a502d8f708
Amy Liu <amy.biyu.liu@gmail.com>
2024-02-27 15:29:33 +1030

    update 4xx test case not to check exit program

--- internal/api/test_plan_test.go
+++ internal/api/test_plan_test.go
@@ -2,12 +2,8 @@ package api
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -15,39 +11,28 @@ import (
 
 func ptr[T any](t T) *T { return &t }
 
-// Referencing this post to test program exit with status 1 https://blog.antoine-augusti.fr/2015/12/testing-an-os-exit-scenario-in-golang/
 func TestFetchTestPlan_Error4xx(t *testing.T) {
 	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
 	}))
 	defer svr.Close()
 
-	// Only run the failing part when a specific env variable is set
-	if os.Getenv("4XX_ERROR") == "1" {
-		params := TestPlanParams{}
-		FetchTestPlan(svr.URL, params)
-		return
+	params := TestPlanParams{}
+	got, err := FetchTestPlan(svr.URL, params)
+
+	wantTestPlan := TestPlan{}
+	wantErrorMessage := "server response: 400"
+
+	if err == nil {
+		t.Errorf("FetchTestPlan(%q, %v) should return an error", svr.URL, params)
 	}
 
-	// Start the actual test in a different subprocess
-	cmd := exec.Command(os.Args[0], "-test.run=TestFetchTestPlan_Error4xx")
-	stdout, _ := cmd.StderrPipe()
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
+	if diff := cmp.Diff(got, wantTestPlan); diff != "" {
+		t.Errorf("FetchTestPlan(%q, %v) diff (-got +want):\n%s", svr.URL, params, diff)
 	}
 
-	// Check that the log fatal message is what we expected
-	gotBytes, _ := io.ReadAll(stdout)
-	got := string(gotBytes)
-	expected := "Cannot process the request"
-	if !strings.HasSuffix(got[:len(got)-1], expected) {
-		t.Fatalf("Unexpected log message. Got %s but should contain %s", got[:len(got)-1], expected)
-	}
-
-	// Check that the program exited
-	err := cmd.Wait()
-	if e, ok := err.(*exec.ExitError); !ok || e.Success() {
-		t.Fatalf("Process ran with err %v, want exit status 1", err)
+	if diff := cmp.Diff(err.Error(), wantErrorMessage); diff != "" {
+		t.Errorf("FetchTestPlan(%q, %v) diff (-got +want):\n%s", svr.URL, params, diff)
 	}
 }
 
