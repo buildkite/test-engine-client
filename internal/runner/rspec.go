f0efcc74085481591abf85baf70c6c220b1c221c
Naufan Rizal <naufan@buildkite.com>
2024-03-21 12:49:28 +0700

    remove test execution report

--- internal/runner/rspec.go
+++ internal/runner/rspec.go
@@ -1,17 +1,11 @@
 package runner
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
-	"time"
-
-	"github.com/buildkite/test-splitter/internal/plan"
 )
 
 // In future, Rspec will implement an interface that defines
@@ -59,115 +53,4 @@ func (Rspec) Command(testCases []string) *exec.Cmd {
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd
-}
-
-// RspecExample defines metadata for an rspec example (test).
-type RspecExample struct {
-	Id              string  `json:"id"`
-	Description     string  `json:"description"`
-	FullDescription string  `json:"full_description"`
-	Status          string  `json:"status"`
-	FilePath        string  `json:"file_path"`
-	LineNumber      int     `json:"line_number"`
-	RunTime         float64 `json:"run_time"`
-}
-
-// RspecReport defines metadata for an rspec generated test report.
-type RspecReport struct {
-	Version  string         `json:"version"`
-	Seed     int            `json:"seed"`
-	Examples []RspecExample `json:"examples"`
-}
-
-// Report produces a report that surfaces estimated and actual
-// performance data for Rspec tests files.
-//
-// This is helpful for us in development, not sure if we will
-// continue to maintain this functionality.
-func (r Rspec) Report(w io.Writer, testCases []plan.TestCase) error {
-	// get all rspec json files
-	reportFiles, err := filepath.Glob("tmp/rspec-*.json")
-	if err != nil {
-		return fmt.Errorf("globbing for report files: %v", err)
-	}
-
-	// early return if no report files
-	if len(reportFiles) == 0 {
-		fmt.Fprintf(w, "No report files found")
-		return nil
-	}
-
-	// read and parse all rspec json files
-	var reports []RspecReport
-	var errs []error
-	for _, reportFile := range reportFiles {
-		var report RspecReport
-		if err := readJsonFile(reportFile, &report); err != nil {
-			errs = append(errs, err)
-			continue
-		}
-		reports = append(reports, report)
-	}
-	if len(errs) > 0 {
-		return fmt.Errorf("reading report files: %v", errors.Join(errs...))
-	}
-
-	// aggregate execution time by file
-	executionByFile := make(map[string]float64)
-	for _, report := range reports {
-		for _, example := range report.Examples {
-			fileName := strings.Replace(example.FilePath, "./", "", 1)
-			executionByFile[fileName] += example.RunTime
-		}
-	}
-
-	// print report
-
-	// calculate width for each column
-	fileNameWidth := 0
-	for _, testCase := range testCases {
-		if len(testCase.Path) > fileNameWidth {
-			fileNameWidth = len(testCase.Path)
-		}
-	}
-	estimatedDurationWidth := len("Estimated")
-	actualDurationWidth := len("Actual")
-	predictionErrorWidth := len("Error %")
-
-	// print header
-	dashedLine := strings.Repeat("-", fileNameWidth+estimatedDurationWidth+actualDurationWidth+predictionErrorWidth+11) + "\n"
-	fmt.Fprintf(w, dashedLine)
-	fmt.Fprintf(w,
-		"%-*s | %-*s | %-*s | %-*s |\n",
-		fileNameWidth, "File Name",
-		estimatedDurationWidth, "Estimated",
-		actualDurationWidth, "Actual",
-		predictionErrorWidth, "Error %",
-	)
-	fmt.Fprintf(w, dashedLine)
-
-	// print each row
-	for _, testCase := range testCases {
-
-		var estimatedDuration time.Duration
-
-		if testCase.EstimatedDuration != nil {
-			estimatedDuration = time.Duration(*testCase.EstimatedDuration) * time.Microsecond
-		}
-		// Actual duration from rspec report is in second
-		actualDuration := executionByFile[testCase.Path] * float64(time.Second)
-
-		predictionError := math.Abs((actualDuration - float64(estimatedDuration)) / actualDuration * 100)
-
-		fmt.Fprintf(w,
-			"%-*s | %*s | %*s | %*s |\n",
-			fileNameWidth, testCase.Path,
-			estimatedDurationWidth, estimatedDuration.Truncate(time.Millisecond).String(),
-			actualDurationWidth, time.Duration(actualDuration).Truncate(time.Millisecond).String(),
-			predictionErrorWidth, fmt.Sprintf("%.2f%%", predictionError),
-		)
-	}
-	fmt.Fprintf(w, dashedLine)
-
-	return nil
 }
