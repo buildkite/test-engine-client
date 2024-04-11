e20676043742fca84098c543cab54dc731bfac1f
Amy Liu <amy.biyu.liu@gmail.com>
2024-04-11 10:02:35 +0930

    add retry and update comments

--- .buildkite/steps/install-binary.sh
+++ .buildkite/steps/install-binary.sh
@@ -4,9 +4,7 @@ set -euo pipefail
 
 echo -e "--- :wrench: Install Test Splitter"
 
-# if the build is triggered by Test Splitter Client pipeline,
-# download the test-splitter binary from the triggering build,
-# otherwise install the test-splitter from the GitHub.
+# Download go binary from artifact
 
 echo -e "Downloading test-splitter from the test-splitter-client build artifact"
 buildkite-agent artifact download pkg/test-splitter-linux-amd64 .
@@ -22,10 +20,11 @@ set +e
 
 echo -e "+++ :rspec: Running specs"
 
-cat "${ERROR_PLAN:-}"
+# Update buildkite build id when we run e2e tests for server return 
+# error plan scenario
+
 if [[ "${ERROR_PLAN:-}" == "true" ]] ; then
   BUILDKITE_BUILD_ID=error_plan_build_id
-  cat "${BUILDKITE_BUILD_ID}"
 fi
 
 ./test-splitter
@@ -33,3 +32,25 @@ fi
 exit_code=$?
 echo -e "--- :bangbang: RSpec exit code: $exit_code"
 set -e
+
+# Retry failures up to 3 times
+retry_count=1
+max_retries=3
+
+while [ $exit_code -ne 0 ] && [ $retry_count -le $max_retries ]
+do
+  echo -e "+++ :recycle: Attempt $retry_count of $max_retries to retry failing tests"
+
+  # Retry failures by calling RSpec directly
+  set +e
+  bin/rspec --options .rspec.ci --seed ${BUILDKITE_SEED:-${BUILDKITE_BUILD_NUMBER:-$RANDOM}} --only-failures
+  exit_code=$?
+  set -e
+
+  if [ $exit_code -ne 0 ] && [ $retry_count -eq $max_retries ]; then
+    echo -e "--- :red_circle: Tests failed"
+    exit $exit_code
+  fi
+
+  ((retry_count++))
+done
