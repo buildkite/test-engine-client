74100662018ea2b9abe51982d7f8fe662840342b
Naufan Rizal <naufan@buildkite.com>
2024-04-18 13:38:24 +1200

    rename test folder to fixtures

--- internal/runner/discover_test.go
+++ internal/runner/discover_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestDiscoverTestFiles(t *testing.T) {
-	pattern := "test/**/*_test"
+	pattern := "fixtures/**/*_test"
 	got, err := discoverTestFiles(pattern, "")
 
 	if err != nil {
@@ -15,11 +15,11 @@ func TestDiscoverTestFiles(t *testing.T) {
 	}
 
 	want := []string{
-		"test/animals/ant_test",
-		"test/animals/bee_test",
-		"test/fruits/apple_test",
-		"test/fruits/banana_test",
-		"test/vegetable_test",
+		"fixtures/animals/ant_test",
+		"fixtures/animals/bee_test",
+		"fixtures/fruits/apple_test",
+		"fixtures/fruits/banana_test",
+		"fixtures/vegetable_test",
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
@@ -28,8 +28,8 @@ func TestDiscoverTestFiles(t *testing.T) {
 }
 
 func TestDiscoverTestFiles_WithExcludePattern(t *testing.T) {
-	pattern := "test/**/*_test"
-	excludePattern := "test/animals/*"
+	pattern := "fixtures/**/*_test"
+	excludePattern := "fixtures/animals/*"
 	got, err := discoverTestFiles(pattern, excludePattern)
 
 	if err != nil {
@@ -37,9 +37,9 @@ func TestDiscoverTestFiles_WithExcludePattern(t *testing.T) {
 	}
 
 	want := []string{
-		"test/fruits/apple_test",
-		"test/fruits/banana_test",
-		"test/vegetable_test",
+		"fixtures/fruits/apple_test",
+		"fixtures/fruits/banana_test",
+		"fixtures/vegetable_test",
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
