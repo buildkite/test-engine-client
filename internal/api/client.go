127c33485747668ba8fe98215fba49b251e1a154
Naufan Rizal <naufan@buildkite.com>
2024-05-15 11:10:04 +1200

    Tidied up api client

--- internal/api/client.go
+++ internal/api/client.go
@@ -4,28 +4,34 @@ import (
 	"net/http"
 )
 
+// client is a client for the test splitter API.
+// It contains the organization slug, server base URL, and an HTTP client.
 type client struct {
 	OrganizationSlug string
 	ServerBaseUrl    string
-	AccessToken      string
 	httpClient       *http.Client
 }
 
+// ClientConfig is the configuration for the test splitter API client.
 type ClientConfig struct {
 	AccessToken      string
 	OrganizationSlug string
 	ServerBaseUrl    string
 }
 
+// authTransport is a middleware for the HTTP client.
 type authTransport struct {
 	accessToken string
 }
 
+// RoundTrip adds the Authorization header to all requests made by the HTTP client.
 func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "Bearer "+t.accessToken)
 	return http.DefaultTransport.RoundTrip(req)
 }
 
+// NewClient creates a new client for the test splitter API with the given configuration.
+// It also creates an HTTP client with an authTransport middleware.
 func NewClient(cfg ClientConfig) *client {
 	httpClient := &http.Client{
 		Transport: &authTransport{
@@ -35,7 +41,6 @@ func NewClient(cfg ClientConfig) *client {
 
 	return &client{
 		OrganizationSlug: cfg.OrganizationSlug,
-		AccessToken:      cfg.AccessToken,
 		ServerBaseUrl:    cfg.ServerBaseUrl,
 		httpClient:       httpClient,
 	}
