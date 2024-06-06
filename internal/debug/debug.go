bbbc971c414d18c467815d25dbc4991bfd008625
Naufan Rizal <naufan@buildkite.com>
2024-06-06 15:07:32 +1200

    create debug package

---
+++ internal/debug/debug.go
@@ -0,0 +1,32 @@
+package debug
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+var enabled = false
+var logger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags|log.Lmsgprefix)
+
+// Printf works like log.Printf, but only when debugging is enabled.
+func Printf(format string, v ...interface{}) {
+	if enabled {
+		logger.Printf(format, v...)
+	}
+}
+
+// Println works like log.Println, but only when debugging is enabled.
+func Println(v ...interface{}) {
+	if enabled {
+		logger.Println(v...)
+	}
+}
+
+func SetDebug(b bool) {
+	enabled = b
+}
+
+func SetOutput(w io.Writer) {
+	logger.SetOutput(w)
+}
