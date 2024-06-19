327db7d1572498843a0d28692f19daebc17ab3c2
Naufan Rizal <naufan@buildkite.com>
2024-06-19 14:42:52 +1200

    create DoWithRetry function in the api.Client

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,10 +1,21 @@
 package api
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime"
+	"strconv"
+	"time"
+
+	"github.com/buildkite/roko"
+	"github.com/buildkite/test-splitter/internal/debug"
 )
+
+var ErrRetryTimeout = errors.New("request retry timeout")
 
 // client is a client for the test splitter API.
 // It contains the organization slug, server base URL, and an HTTP client.
@@ -32,7 +43,6 @@ type authTransport struct {
 func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("Authorization", "Bearer "+t.accessToken)
 	req.Header.Set("User-Agent", fmt.Sprintf("Buildkite Test Splitter/%s (%s/%s)", t.version, runtime.GOOS, runtime.GOARCH))
-
 	return http.DefaultTransport.RoundTrip(req)
 }
 
@@ -51,4 +61,81 @@ func NewClient(cfg ClientConfig) *Client {
 		ServerBaseUrl:    cfg.ServerBaseUrl,
 		httpClient:       httpClient,
 	}
+}
+
+var retryTimeout = 130 * time.Second
+var initialDelay = 3000 * time.Millisecond
+
+// DoWithRetry sends http request with retries.
+// It retries on request errors (e.g. redirects, protocol errors), 429, and 5xx status codes.
+// It uses an exponential backoff strategy with jitter, except for 429 status codes where it waits until the rate limit resets.
+// DeadlineExceeded error is returned if the request cannot be fulfilled within the retry timeout which is 130 seconds.
+// 130 seconds is chosen to allow for 2 API rate limit windows.
+func (c *Client) DoWithRetry(ctx context.Context, method string, url string, body any) (*http.Response, error) {
+	r := roko.NewRetrier(
+		roko.TryForever(),
+		roko.WithStrategy(roko.ExponentialSubsecond(initialDelay)),
+		roko.WithJitter(),
+	)
+
+	retryContext, cancel := context.WithTimeout(ctx, retryTimeout)
+	defer cancel()
+
+	debug.Printf("Sending request %s %s", method, url)
+	resp, err := roko.DoFunc(retryContext, r, func(r *roko.Retrier) (*http.Response, error) {
+		if r.AttemptCount() > 0 {
+			debug.Printf("Retrying requests, attempt %d", r.AttemptCount())
+		}
+
+		reqBody, err := json.Marshal(body)
+		if err != nil {
+			r.Break()
+			return nil, fmt.Errorf("converting body to json: %w", err)
+		}
+
+		req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
+		if err != nil {
+			r.Break()
+			return nil, fmt.Errorf("creating request: %w", err)
+		}
+		req.Header.Add("Content-Type", "application/json")
+
+		resp, err := c.httpClient.Do(req)
+
+		// If we get an error before getting a response,
+		// which means there is a redirect or protocol error,
+		// we should retry.
+		if err != nil {
+			debug.Printf("Error sending request: %v", err)
+			return nil, err
+		}
+
+		debug.Printf("Response code %d", resp.StatusCode)
+
+		// If we get a 429, we should wait until the rate limit resets and retry.
+		if resp.StatusCode == http.StatusTooManyRequests {
+			if rateLimitReset, err := strconv.Atoi(resp.Header.Get("RateLimit-Reset")); err == nil {
+				r.SetNextInterval(time.Duration(rateLimitReset) * time.Second)
+			}
+			return resp, fmt.Errorf("response code: 429")
+		}
+
+		// If we get a 5xx, we should retry
+		if resp.StatusCode >= 500 {
+			return resp, fmt.Errorf("response code: %d", resp.StatusCode)
+		}
+
+		// If we get a 4xx, we should not retry
+		if resp.StatusCode >= 400 {
+			r.Break()
+		}
+
+		return resp, nil
+	})
+
+	if errors.Is(err, context.DeadlineExceeded) {
+		return resp, ErrRetryTimeout
+	}
+
+	return resp, err
 }
--- internal/api/client_test.go
+++ internal/api/client_test.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestNewClient(t *testing.T) {
@@ -72,5 +77,187 @@ func TestHttpClient_AttachUserAgentToRequest(t *testing.T) {
 
 	if !strings.Contains(resp.Request.Header.Get("User-Agent"), "Buildkite Test Splitter/0.5.1") {
 		t.Errorf("User-agent header = %v, want %v", resp.Request.Header.Get("User-Agent"), "Buildkite Test Splitter/0.5.1 ...")
+	}
+}
+
+func TestDoWithRetry_Success(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.Copy(w, r.Body)
+	}))
+	defer svr.Close()
+
+	cfg := ClientConfig{
+		ServerBaseUrl: svr.URL,
+	}
+	c := NewClient(cfg)
+
+	resp, err := c.DoWithRetry(context.Background(), http.MethodPost, svr.URL, map[string]string{"message": "hello"})
+
+	if err != nil {
+		t.Errorf("DoWithRetry(ctx, req) error = %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("DoWithRetry(ctx, req) status code = %v, want %v", resp.StatusCode, http.StatusOK)
+	}
+
+	responseBody, _ := io.ReadAll(resp.Body)
+	want := `{"message":"hello"}`
+	if string(responseBody) != want {
+		t.Errorf("DoWithRetry(ctx, req) body = %v, want %v", string(responseBody), want)
+	}
+}
+
+func TestDoWithRetry_RequestError(t *testing.T) {
+	originalTimeout := retryTimeout
+	retryTimeout = 300 * time.Millisecond
+	t.Cleanup(func() {
+		retryTimeout = originalTimeout
+	})
+
+	cfg := ClientConfig{
+		AccessToken:      "asdf1234",
+		OrganizationSlug: "my-org",
+	}
+
+	c := NewClient(cfg)
+	resp, err := c.DoWithRetry(context.Background(), http.MethodGet, "http://build.kite", nil)
+
+	fmt.Println(resp)
+
+	// it retries the request and returns ErrRetryTimeout with nil response.
+	if !errors.Is(err, ErrRetryTimeout) {
+		t.Errorf("DoWithRetry(ctx, req) error = %v, want %v", err, ErrRetryTimeout)
+	}
+
+	if resp != nil {
+		t.Errorf("DoWithRetry(ctx, req) = %v, want nil", resp)
+	}
+}
+
+func TestDoWithRetry_429(t *testing.T) {
+	originalTimeout := retryTimeout
+	retryTimeout = 2 * time.Second
+	t.Cleanup(func() {
+		retryTimeout = originalTimeout
+	})
+
+	callCount := 0
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		callCount++
+		w.Header().Set("RateLimit-Reset", "1")
+		w.WriteHeader(http.StatusTooManyRequests)
+	}))
+	defer svr.Close()
+
+	cfg := ClientConfig{
+		AccessToken:      "asdf1234",
+		OrganizationSlug: "my-org",
+		ServerBaseUrl:    svr.URL,
+	}
+
+	c := NewClient(cfg)
+	resp, err := c.DoWithRetry(context.Background(), http.MethodGet, svr.URL, nil)
+
+	// it retries the request and returns ErrRetryTimeout with the 429 status code.
+	if callCount != 2 {
+		t.Errorf("http request count = %v, want %v", callCount, 2)
+	}
+
+	if !errors.Is(err, ErrRetryTimeout) {
+		t.Errorf("DoWithRetry(ctx, req) error = %v, want %v", err, ErrRetryTimeout)
+	}
+
+	if resp.StatusCode != http.StatusTooManyRequests {
+		t.Errorf("DoWithRetry(ctx, req) status code = %v, want %v", resp.StatusCode, http.StatusTooManyRequests)
+	}
+}
+
+func TestDoWithRetry_500(t *testing.T) {
+	originalTimeout := retryTimeout
+	originalInitialDelay := initialDelay
+
+	retryTimeout = 2 * time.Second
+	initialDelay = 100 * time.Millisecond
+	t.Cleanup(func() {
+		retryTimeout = originalTimeout
+		initialDelay = originalInitialDelay
+	})
+
+	callCount := 0
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		callCount++
+
+		w.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer svr.Close()
+
+	cfg := ClientConfig{
+		AccessToken:      "asdf1234",
+		OrganizationSlug: "my-org",
+		ServerBaseUrl:    svr.URL,
+	}
+
+	c := NewClient(cfg)
+
+	resp, err := c.DoWithRetry(context.Background(), http.MethodPost, svr.URL, nil)
+
+	// it retries the request and returns ErrRetryTimeout with the 500 status code.
+	if callCount < 2 {
+		t.Errorf("http request count = %v, want at least 2", callCount)
+	}
+
+	if !errors.Is(err, ErrRetryTimeout) {
+		t.Errorf("DoWithRetry(ctx, req) error = %v, want %v", err, ErrRetryTimeout)
+	}
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("DoWithRetry(ctx, req) status code = %v, want %v", resp.StatusCode, http.StatusInternalServerError)
+	}
+}
+
+func TestDoWithRetry_403(t *testing.T) {
+	originalTimeout := retryTimeout
+	originalInitialDelay := initialDelay
+
+	retryTimeout = 500 * time.Millisecond
+	initialDelay = 100 * time.Millisecond
+
+	t.Cleanup(func() {
+		retryTimeout = originalTimeout
+		initialDelay = originalInitialDelay
+	})
+
+	callCount := 0
+
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		callCount++
+		w.WriteHeader(http.StatusForbidden)
+	}))
+	defer svr.Close()
+
+	cfg := ClientConfig{
+		AccessToken:      "asdf1234",
+		OrganizationSlug: "my-org",
+		ServerBaseUrl:    svr.URL,
+	}
+
+	c := NewClient(cfg)
+	resp, err := c.DoWithRetry(context.Background(), http.MethodGet, svr.URL, nil)
+
+	// it returns immediately with the 403 status code.
+	if callCount > 1 {
+		t.Errorf("http request count = %v, want 1", callCount)
+	}
+
+	if err != nil {
+		t.Errorf("DoWithRetry(ctx, req) error = %v", err)
+	}
+
+	if resp.StatusCode != http.StatusForbidden {
+		t.Errorf("DoWithRetry(ctx, req) status code = %v, want %v", resp.StatusCode, http.StatusForbidden)
 	}
 }
