a4cb0cc3a362ba2cb1747fac83c6e1d8a209581d
Naufan Rizal <naufan@buildkite.com>
2024-07-24 17:18:11 +1200

    push to Dockerhub

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -4,11 +4,14 @@ tag=$(buildkite-agent meta-data get "release-version")
 
 git tag "${tag}"
 
-echo "--- :key: Login to Buildkite Packages"
+echo "--- :key: :buildkite: Login to Buildkite Packages"
  buildkite-agent oidc request-token \
    --audience "https://packages.buildkite.com/buildkite/test-splitter-docker" \
    --lifetime 300 \
    | docker login packages.buildkite.com/buildkite/test-splitter-docker --username=buildkite --password-stdin
 
-echo "--- :github: Creating Release"
+echo "--- :key: :docker: Login to Docker"
+echo "${DOCKERHUB_PASSWORD}" | docker login --username "${DOCKERHUB_USER}" --password-stdin
+
+echo "--- :rocket: Creating Release"
 goreleaser release --clean
