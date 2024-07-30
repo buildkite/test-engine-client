6febee66bd0905ce882f3ac7c6293fbc88b66416
Naufan Rizal <naufan@buildkite.com>
2024-07-31 11:10:33 +1200

    publish linux packages

---
+++ .buildkite/steps/upload-linux-packages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+file=${1}
+extension="${file##*.}"
+registry="test-splitter-${extension}"
+audience="https://packages.buildkite.com/buildkite/${registry}"
+
+echo "--- :key: :buildkite: Fetching OIDC token for ${audience}"
+token=$(buildkite-agent oidc request-token --audience "${audience}" --lifetime 180)
+
+echo "--- :linux: Uploading ${file}"
+curl -s -X POST "https://api.buildkite.com/v2/packages/organizations/buildkite/registries/${registry}/packages" \
+      -H "Authorization: Bearer ${token}" \
+      -F "file=@${file}" \
+    --fail-with-body
