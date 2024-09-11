8f5aadc5396bee997e02e0eb638cee2fbdb8f251
Naufan Rizal <naufan@buildkite.com>
2024-09-11 17:48:44 +1200

    Ignore -rc tags when releasing stable version

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -4,6 +4,22 @@ tag=$(buildkite-agent meta-data get "release-version")
 
 git tag "${tag}"
 
+# When releasing a stable version, we want the changelog
+# in GitHub to be based on the previous stable version.
+#
+# For illustrations, consider the following tags:
+# - v1.0.0
+# - v1.0.1-rc.1
+# - v1.0.1-rc.2
+#
+# When releasing v1.0.1, we want the changelog to be based on v1.0.0, not v1.0.1-rc.2.
+# Hoever, when releasing v1.0.1-rc.3, we want the changelog to be based on v1.0.1-rc.2.
+#
+# To do this, we need to ignore all '-rc' tags when releasing a stable version.
+if [[ ! "${tag}" =~ "-rc" ]]; then
+  export GORELEASER_IGNORE_TAG="*-rc.*"
+fi
+
 echo "--- :key: :buildkite: Login to Buildkite Packages"
  buildkite-agent oidc request-token \
    --audience "https://packages.buildkite.com/buildkite/test-splitter-docker" \
