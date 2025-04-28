3977f5fd21cbd1fa82a2bc988aa633a8ea448293
Ming <ming@buildkite.com>
2025-04-28 15:51:16 +1000

    TE-3707: use bktec on bktec pipeline

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -18,7 +18,14 @@ yarn playwright install
 cd ../../../..
 
 echo '+++ Running tests'
-gotestsum --junitfile "junit-${BUILDKITE_JOB_ID}.xml" -- -count=1 -coverprofile=cover.out -failfast "$@" ./...
+
+export BUILDKITE_TEST_ENGINE_SUITE_SLUG=bktec
+export BUILDKITE_TEST_ENGINE_TEST_RUNNER=gotest
+export BUILDKITE_TEST_ENGINE_RESULT_PATH="junit-${BUILDKITE_JOB_ID}.xml"
+export BUILDKITE_TEST_ENGINE_RETRY_COUNT=1
+export BUILDKITE_TEST_ENGINE_TEST_CMD='gotestsum --junitfile={{resultPath}} -- -count=1 -coverprofile=cover.out -failfast -race {{packages}}'
+
+bktec
 
 echo 'Producing coverage report'
 go tool cover -html cover.out -o cover.html
