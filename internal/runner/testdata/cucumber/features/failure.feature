63cac56a40783ce6379a680ad731f6572eb8a638
Tom Meier <tom@venombytes.com>
2025-06-04 14:41:15 +1000

    Add failure definitions also

---
+++ internal/runner/testdata/cucumber/features/failure.feature
@@ -0,0 +1,4 @@
+Feature: Failing Test
+  Scenario: This scenario is supposed to fail
+    Given A functioning wand
+    Then Something unexpected happens that causes a failure
--- internal/runner/testdata/cucumber/features/step_definitions/steps.rb
+++ internal/runner/testdata/cucumber/features/step_definitions/steps.rb
@@ -5,3 +5,7 @@ end
 Then("Opponent is disarmed") do
   true
 end
+
+Then("Something unexpected happens that causes a failure") do
+  raise "This step is designed to fail!"
+end
