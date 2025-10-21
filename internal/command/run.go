e3069c1202d38acde62cc4307b169b9888d037c0
Malcolm Locke <malcolm@buildkite.com>
2025-10-21 15:10:05 +1300

    Move command.createRequestParam to separate file
    This method is used in both `command.Run` and `command.Plan`.

--- internal/command/run.go
+++ internal/command/run.go
@@ -362,63 +362,6 @@ func fetchOrCreateTestPlan(ctx context.Context, apiClient *api.Client, cfg confi
 	return testPlan, nil
 }
 
-// createRequestParam generates the parameters needed for a test plan request.
-// For runners other than "rspec", it constructs the test plan parameters with all test files.
-// For the "rspec" runner, it filters the test files through the Test Engine API and splits the filtered files into examples.
-func createRequestParam(ctx context.Context, cfg config.Config, files []string, client api.Client, runner TestRunner) (api.TestPlanParams, error) {
-	testFiles := []plan.TestCase{}
-	for _, file := range files {
-		testFiles = append(testFiles, plan.TestCase{
-			Path: file,
-		})
-	}
-
-	// Splitting files by example is only supported for rspec runner & cucumber
-	if runner.Name() != "RSpec" && runner.Name() != "Cucumber" {
-		params := api.TestPlanParams{
-			Identifier:     cfg.Identifier,
-			Parallelism:    cfg.Parallelism,
-			MaxParallelism: cfg.MaxParallelism,
-			Branch:         cfg.Branch,
-			Runner:         cfg.TestRunner,
-			Tests: api.TestPlanParamsTest{
-				Files: testFiles,
-			},
-		}
-
-		// This is a workaround for the fact that the pytest-pants runner is not
-		// supported by the Test Engine API. For now, we use the pytest runner. At
-		// some point, there may be a difference between the two runners, but for
-		// now the response from the Test Engine API is the same for both runners.
-		if cfg.TestRunner == "pytest-pants" {
-			params.Runner = "pytest"
-		}
-
-		return params, nil
-	}
-
-	if cfg.SplitByExample {
-		debug.Println("Splitting by example")
-	}
-
-	// The SplitByExample flag indicates whether to filter slow files for splitting by example.
-	// Regardless of the flag's state, the API will still filter other files that need to be split by example, such as those containing skipped tests.
-	// Therefore, we must filter and split files even when SplitByExample is disabled.
-	testParams, err := filterAndSplitFiles(ctx, cfg, client, testFiles, runner)
-	if err != nil {
-		return api.TestPlanParams{}, err
-	}
-
-	return api.TestPlanParams{
-		Identifier:     cfg.Identifier,
-		Parallelism:    cfg.Parallelism,
-		MaxParallelism: cfg.MaxParallelism,
-		Branch:         cfg.Branch,
-		Runner:         cfg.TestRunner,
-		Tests:          testParams,
-	}, nil
-}
-
 // filterAndSplitFiles filters the test files through the Test Engine API and splits the filtered files into examples.
 // It returns the test plan parameters with the examples from the filtered files and the remaining files.
 // An error is returned if there is a failure in any of the process.
