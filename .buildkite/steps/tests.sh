63e25f549345e5372be2dc996a65c0064df87f5c
Matt Ensor <matt.ensor@buildkite.com>
2026-06-26 15:10:11 +1000

    ci: use head bktec for test splitting on main

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -13,18 +13,10 @@ cd playwright
 yarn playwright install
 cd ../../../..
 
-# On feature branches, build bktec from the current source so the pipeline
-# splits its own tests using the code under test. This way a change that breaks
-# bktec's test splitting fails the pipeline before it can be merged to main.
-# On main we use the released binary installed in the image (see Dockerfile).
-bktec_bin="bktec"
-if [[ "${BUILDKITE_BRANCH:-}" != "main" ]]; then
-  echo '+++ Building bktec from source'
-  go build \
-    -ldflags "-X 'github.com/buildkite/test-engine-client/v2/internal/version.Version=${BUILDKITE_COMMIT:-dev}'" \
-    -o /tmp/bktec .
-  bktec_bin="/tmp/bktec"
-fi
+echo '+++ Building bktec from source'
+go build \
+  -ldflags "-X 'github.com/buildkite/test-engine-client/v2/internal/version.Version=${BUILDKITE_COMMIT:-dev}'" \
+  -o /tmp/bktec .
 
 echo '+++ Running tests'
 
@@ -35,7 +27,7 @@ export BUILDKITE_TEST_ENGINE_RETRY_COUNT=1
 export BUILDKITE_TEST_ENGINE_TEST_CMD='gotestsum --junitfile={{resultPath}} -- -count=1 -coverprofile=cover.out -failfast -race {{packages}}'
 export BUILDKITE_TEST_ENGINE_UPLOAD_RESULTS=true
 
-"${bktec_bin}" run
+/tmp/bktec run
 
 echo 'Producing coverage report'
 go tool cover -html cover.out -o cover.html
