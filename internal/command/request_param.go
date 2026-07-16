2b273f8e401f7c20830694d7d47c01d68d481e65
Meghan Kradolfer <meghan@buildkite.com>
2026-07-16 16:54:27 +1200

    [TE-6386] Support split by example with selector splitting

--- internal/command/request_param.go
+++ internal/command/request_param.go
@@ -25,7 +25,7 @@ import (
 // Currently only the Pytest runner supports tag filtering.
 func createRequestParam(ctx context.Context, cfg *config.Config, testTargets []string, client api.Client, runner runner.TestRunner) (api.TestPlanParams, error) {
 	if shouldUseSelectorSplitting(cfg, runner) {
-		if shouldExpandSelectorFilesForSkippedTests(runner) {
+		if shouldExpandSelectorFiles(cfg, runner) {
 			testParams, err := filterAndSplitSelectorFiles(ctx, cfg, client, testTargets, runner)
 			if err != nil {
 				return api.TestPlanParams{}, err
@@ -137,9 +137,9 @@ func shouldUseSelectorSplitting(cfg *config.Config, runner runner.TestRunner) bo
 	return cfg.SelectorSplitting && runner.SupportedFeatures().SplitBySelector
 }
 
-func shouldExpandSelectorFilesForSkippedTests(runner runner.TestRunner) bool {
+func shouldExpandSelectorFiles(cfg *config.Config, runner runner.TestRunner) bool {
 	features := runner.SupportedFeatures()
-	return features.SplitBySelector && features.SplitByExample && features.FilterTestFiles && features.Skip
+	return features.SplitBySelector && features.SplitByExample && features.FilterTestFiles && (features.Skip || cfg.SplitByExample)
 }
 
 func selectorParamsFromValues(values []string) []api.TestPlanParamsSelector {
